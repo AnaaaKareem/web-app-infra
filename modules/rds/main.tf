6829a3ca52ce8692a384028ce9576fd765592834
Kareem Khater <118448251+AnaaaKareem@users.noreply.github.com>
2025-08-21 17:12:10 +0300

    Update main.tf rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -57,9 +57,9 @@ resource "aws_security_group" "db" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
